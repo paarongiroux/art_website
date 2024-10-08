fff41a498a5d002eb5df245531a2df2a98628fa5
Aaron <pag87@nau.edu>
2024-10-08 16:58:14 -0700

    convert about page to threejs

--- src/about.js
+++ src/about.js
@@ -1,226 +1,220 @@
 import React from 'react';
 import NavBar from './NavBar';
 import './App.css';
-import vertexShaderSrc from './vertexShader.js';
-import fragmentShaderSrc from './fragmentShader.js';
-import aviModel from './body.json';
-import aviTexture from './avi_flo.jpg';
-import * as glMatrix from 'gl-matrix';
-const { mat4 } = glMatrix;
+
+import { useEffect, useRef } from "react";
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
 
 function About() {
-    var canvasWidth = window.innerWidth * 0.9;
-    var canvasHeight = 600;
-    if (canvasWidth > 800) {
-        canvasWidth = 800
-    } else {
-        canvasHeight = canvasWidth * 0.75;
-    }
-    console.log(canvasWidth);
-    return (
-        <div onLoad= {initDemo}>
-            <NavBar/>
-            <div className="imgContainer">
-            <div className="about">
-                ABOUT ME
-            </div>
-            <canvas id="canvas" width={canvasWidth} height="600"></canvas>
-            <div className="about">
-                Age: 26 <br></br>
-                Sex: M <br></br>
-                Height: 182cm <br></br>
-                Wing span: 186cm <br></br>
-                Location: San Diego <br></br>
-                {/* Fav color: <span style="color:blue">blue</span><br></br> */}
-                Contact: p.aaron.giroux@gmail.com <br></br>
-            </div>
-            </div>
-        </div>
-    );
-}
+    const refContainer = useRef(null);
+  useEffect(() => {
+    // === THREE.JS CODE START ===
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight - 50), 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
 
-async function initDemo() {
-    let aviTextureImage = new Image(aviTexture);
-    aviTextureImage.src = aviTexture
-    await new Promise(r => setTimeout(r, 500));;
-    console.log(aviTextureImage);
-    runDemo(vertexShaderSrc, fragmentShaderSrc, aviModel, aviTextureImage);
-}
-
-
-function runDemo(vertexShaderText, fragmentShaderText, headModel, headTextureImg) {
-    // get canvas and webgl context
-    var canvas = document.querySelector("#canvas");
-    var gl = canvas.getContext("webgl");
-    if (!gl) {
-        alert("unable to load webgl");
-    }
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderText);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderText);
-    var program = createProgram(gl, vertexShader, fragmentShader)
-
-    // three 2d points + color
-    var headVertices = headModel.meshes[0].vertices;
-    var headIndices = [].concat.apply([], headModel.meshes[0].faces);
-    var headTexCoords = headModel.meshes[0].texturecoords[0];
-
-    // create and bind boxVertices to ARRAY_BUFFER
-    var headPosVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, headPosVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(headVertices), gl.STATIC_DRAW);
-
-    var headTexCoordVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, headTexCoordVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(headTexCoords), gl.STATIC_DRAW);
-
-    var headIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, headIndexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(headIndices), gl.STATIC_DRAW);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, headPosVertexBuffer);
-    // get position and color attributes.
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    // create attribute pointers 
-    gl.vertexAttribPointer(
-        positionAttributeLocation,
-        3, // num elements per attribute (x, y, z);
-        gl.FLOAT, // type
-        gl.FALSE, //not normalized
-        3 * Float32Array.BYTES_PER_ELEMENT, // size of individual vertex (x,y,z)
-        0 // offset from beginning of a single vertex to this attribute
-    );
-    gl.enableVertexAttribArray(positionAttributeLocation);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, headTexCoordVertexBuffer)
-    var texCoordAttributeLocation = gl.getAttribLocation(program, "vertTexCoord");
-    gl.vertexAttribPointer(
-        texCoordAttributeLocation,
-        2, // num elements per attribute (u, v);
-        gl.FLOAT, // type
-        gl.FALSE, //not normalized
-        2 * Float32Array.BYTES_PER_ELEMENT, // size of individual vertex (x,y)
-        0
-    );
-
-    // enable attrib arrays... for position and color... this tells webgl to use ARRAY_BUFFER with these attributes / pointers
+    renderer.setSize(window.innerWidth, window.innerHeight - 50);
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    refContainer.current && refContainer.current.appendChild( renderer.domElement );
     
-    gl.enableVertexAttribArray(texCoordAttributeLocation);
+    var material =  new THREE.LineBasicMaterial({color: 0x66ff00});
 
-    // create texture
-    var headTexture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, headTexture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texImage2D(
-		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		headTextureImg
-	);
-	gl.bindTexture(gl.TEXTURE_2D, null);
+    const ambLight = new THREE.AmbientLight(0xffffff, 0.7);
+    scene.add(ambLight);
 
-    gl.useProgram(program);
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 2);
+    scene.add(light);
 
-    // enable depth buffering and back face culling when drawing
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
-    gl.frontFace(gl.CCW);
-    gl.cullFace(gl.BACK)
+    camera.position.z = 5;
 
-    var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
-    var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
-    var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+    window.addEventListener('resize', function() {
+        var width = window.innerWidth;
+        var height = window.innerHeight - 50;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    });
 
-    var worldMatrix = new Float32Array(16);
-    var viewMatrix = new Float32Array(16);
-    var projMatrix = new Float32Array(16);
-    mat4.identity(worldMatrix);
-    // generate a camera
-    // mat4.identity(viewMatrix);
-    // mat4.identity(projMatrix);
-    mat4.lookAt(viewMatrix, [0, 0, -3], [0, 0, 0], [0, 1, 0]); // position of view, point the viewer is looking at, vec3 pointing up.
-    mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0)// FOV in radians, aspect ratio (w/h), near plane and far plane
+    var controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
 
-    // 4x4 uniform matrix
-    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+    const loader = new GLTFLoader();
 
-    var xRotationMatrix = new Float32Array(16);
-    var yRotationMatrix = new Float32Array(16);
-
-    // resize and clear.
-    resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(1, 1, 1, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // main render loop
-    var angle = 0;
-    var identityMatrix = new Float32Array(16);
-    mat4.identity(identityMatrix);
+    loader.load( './avi_body.glb', function ( gltf ) {
+        gltf.scene.scale.set(2, 2, 2);
+        gltf.scene.position.y = -1.5;
+        gltf.scene.name = "aaron";
+        scene.add( gltf.scene );
+    }, undefined, function ( error ) {
+    
+        console.error( error );
+    
+    } );
 
 
-    function loop() {
-        angle = performance.now() / 1000 / 3 * 2 * Math.PI; // 1 full rotation (2pi) every 3 seconds
-        mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]); // rotate world about identity by angle around y axis
-        mat4.rotate(xRotationMatrix, identityMatrix, Math.PI * 1.5, [1, 0, 0]); // rotate world about identity by angle around y axis
-        mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
+    const textLoader = new FontLoader();
+    textLoader.load('helvetiker_regular.typeface.json', function (font) {
+        const textGeometry = new TextGeometry("182cm", {
+            font: font,
+            size: 0.2,
+            depth: 0.1
+        });
+        const textMesh = new THREE.Mesh(textGeometry, [
+            new THREE.MeshPhongMaterial({color: 0x66ff00}),
+            new THREE.MeshPhongMaterial({color: 0x0ab30a})
+        ]);
+        textMesh.position.set(1.6, -1.25, 0);
+        textMesh.name = "heightText";
+        scene.add(textMesh);
+
+
+        const textGeometry2 = new TextGeometry("185cm", {
+            font: font,
+            size: 0.2,
+            depth: 0.1
+        });
+        const textMesh2 = new THREE.Mesh(textGeometry2, [
+            new THREE.MeshPhongMaterial({color: 0x66ff00}),
+            new THREE.MeshPhongMaterial({color: 0x0ab30a})
+        ]);
+        textMesh2.position.set(-2.5, -0.75, 0);
+        textMesh2.name = "wSpanText";
+        scene.add(textMesh2);
+
+
+        const statsText = "name: aaron\n" +
+                            "weight: 72.66kg\n" + 
+                            "age: 26\n" + 
+                            "location: california, usa";
+        const textGeometry3 = new TextGeometry(statsText, {
+            font: font,
+            size: 0.2,
+            depth: 0.1
+        });
+        const textMesh3 = new THREE.Mesh(textGeometry3, [
+            new THREE.MeshPhongMaterial({color: 0x66ff00}),
+            new THREE.MeshPhongMaterial({color: 0x0ab30a})
+        ]);
+        textMesh3.position.set(2.5, 2.25, 0);
+        textMesh3.name = "statsText";
+        scene.add(textMesh3);
+
+
+        const contactText = "contact: p.aaron.giroux@gmail.com";
+        const textGeometry4 = new TextGeometry(contactText, {
+            font:font,
+            size: 0.15,
+            depth: 0.1
+        });
+        const textMesh4 = new THREE.Mesh(textGeometry4, [
+            new THREE.MeshPhongMaterial({color: 0xffff00}),
+            new THREE.MeshPhongMaterial({color: 0xa9a913})
+        ]);
+        textMesh4.position.set(-1.65, -1.7, 1.5);
+        textMesh4.name = "contactText";
+        scene.add(textMesh4);
         
-        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    });
 
-        gl.clearColor(0.0, 0.0, 0.0, 0.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        gl.bindTexture(gl.TEXTURE_2D, headTexture);
-		gl.activeTexture(gl.TEXTURE0);
+    // line & line2 are for height 
+    const points = [];
+    var secretPoint = new THREE.Vector3( 1, -0.3, -0.2);
+    points.push( new THREE.Vector3( 0.5, 2.3, -0.2 ) );
+    points.push( new THREE.Vector3( 1, 2.3, -0.2 ) );
+    points.push( secretPoint);
+    points.push( new THREE.Vector3( 1, -1.6, -0.2 ) );
+    points.push( new THREE.Vector3( 0.5, -1.6, -0.2 ) );
+    var geometry = new THREE.BufferGeometry().setFromPoints( points );
 
-        // draww using element array buffer (indices array)
-        gl.drawElements(gl.TRIANGLES, headIndices.length, gl.UNSIGNED_SHORT, 0);
-        requestAnimationFrame(loop);
-    }
-    requestAnimationFrame(loop);
-}
+    const line = new THREE.Line( geometry, material );
+    scene.add( line );
+    
+    var points2 = [];
+    points2.push(secretPoint);
+    points2.push(new THREE.Vector3(2, -1, 0));
+    var geometry2 = new THREE.BufferGeometry().setFromPoints( points2 );
+    const line2 = new THREE.Line(geometry2, material);
+    scene.add(line2);
 
-function createShader(gl, type, source) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-        return shader;
-    }
+    // line 3 & line 4 are for wingspan
+    points.splice(0, points.length);
+    points.push(new THREE.Vector3(-1.8, 1.4, 0.3));
+    points.push(new THREE.Vector3(-1.8, 1.1, 0.3));
+    points.push(new THREE.Vector3(-1.2, 1.1, 0.3));
+    points.push(new THREE.Vector3(1.8, 1.1, 0.3));
+    points.push(new THREE.Vector3(1.8, 1.4, 0.3));
+    geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const line3 = new THREE.Line(geometry, material);
+    scene.add(line3);
 
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-}
+    var points3 = [];
+    points3.push(new THREE.Vector3(-1.2, 1.1, 0.3));
+    points3.push(new THREE.Vector3(-2, -0.5, 0));
+    const geometry3 = new THREE.BufferGeometry().setFromPoints( points3 );
+    const line4 = new THREE.Line(geometry3, material);
+    scene.add(line4);
 
-function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
 
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-}
 
-function resizeCanvasToDisplaySize(canvas, multiplier) {
-    multiplier = multiplier || 1;
-    const width  = canvas.clientWidth  * multiplier | 0;
-    const height = canvas.clientHeight * multiplier | 0;
-    if (canvas.width !== width ||  canvas.height !== height) {
-      canvas.width  = width;
-      canvas.height = height;
-      return true;
-    }
-    return false;
+    
+    
+    var scaleCounter = 0;
+
+    var animate = function () {
+      requestAnimationFrame(animate);
+      scaleCounter = scaleCounter % 360 + 1;
+
+      var scale = 1 + Math.sin(scaleCounter * Math.PI / 180) * 0.1;
+    //   scene.rotation.x += 0.01;
+      var aaronModel = scene.getObjectByName("aaron");
+      var heightText = scene.getObjectByName("heightText");
+      var spanText = scene.getObjectByName("wSpanText");
+
+      if (aaronModel === undefined) {
+        console.log("not yet loaded");
+      } else {
+        aaronModel.rotation.y -= 0.03;
+        line.rotation.y -= 0.03;
+        line3.rotation.y -= 0.03;
+        heightText.scale.set(scale, scale, scale);
+        spanText.scale.set(scale, scale, scale);
+
+
+        var line1Poss = line.geometry.getAttribute("position");
+        var vtx = new THREE.Vector3();
+        vtx.fromBufferAttribute(line1Poss, 2);
+        vtx.applyMatrix4(line.matrixWorld)
+
+        points2[0] = vtx
+        line2.geometry = new THREE.BufferGeometry().setFromPoints( points2 );
+
+        var line3Poss = line3.geometry.getAttribute("position");
+        var vtx2 = new THREE.Vector3();
+        vtx2.fromBufferAttribute(line3Poss, 2);
+        vtx2.applyMatrix4(line3.matrixWorld)
+
+        points3[0] = vtx2
+        line4.geometry = new THREE.BufferGeometry().setFromPoints( points3 );
+      }
+      renderer.render(scene, camera);
+      controls.update();
+    };
+    animate();
+  }, []);
+  return (
+    <div>
+        <NavBar/>
+        <div ref={refContainer}></div>
+    </div>
+  );
 }
 
 
